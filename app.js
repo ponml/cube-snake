0406c953541a08c7c3041045bde8acc8dda9af6f
chris <ccsand@hotmail.com>
2017-02-12 22:53:47 -0800

    bouncing cube

---
+++ app.js
@@ -0,0 +1,35 @@
+var THREE = require("./scripts/three.min.js");
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+var pos = 0.01;
+var render = function () {
+    requestAnimationFrame( render );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    
+    if(cube.position.x < -2) {
+        pos = 0.01;
+    } else if(cube.position.x > 2) {
+        pos = -0.01;
+    }
+    cube.position.x += pos;
+    document.getElementById("info").textContent = cube.position.x;
+
+
+    renderer.render(scene, camera);
+};
+
+render();
